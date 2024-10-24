1a02c57e29d5bf7dd866eea57cb283c7c7d40b37
Milan Hommet <mhommet@teicee.com>
2024-10-24 16:01:05 +0200

    ajust transparency

--- wezterm/lua/appearance.lua
+++ wezterm/lua/appearance.lua
@@ -8,7 +8,7 @@ M.apply_to_config = function(c)
   c.colors = {
     split = scheme.ansi[2],
   }
-  c.window_background_opacity = 1
+  c.window_background_opacity = 0.95
   c.inactive_pane_hsb = { brightness = 0.9 }
   c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
   c.window_decorations = "RESIZE"
