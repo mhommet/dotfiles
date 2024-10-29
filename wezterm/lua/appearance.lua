6ef6b05071979bcf5f2956e027d2a4f6269fe6c9
Milan Hommet <mhommet@teicee.com>
2024-10-29 15:18:23 +0100

    m

--- wezterm/lua/appearance.lua
+++ wezterm/lua/appearance.lua
@@ -3,16 +3,12 @@ local wez = require "wezterm"
 local M = {}
 
 M.apply_to_config = function(c)
-  c.color_scheme = "rose-pine"
-  local scheme = wez.color.get_builtin_schemes()["rose-pine"]
-  c.colors = {
-    split = scheme.ansi[2],
-  }
-  c.window_background_opacity = 0.95
+  c.color_scheme = 'Catppuccin Mocha';
+  c.window_background_opacity = 1
   c.inactive_pane_hsb = { brightness = 0.9 }
   c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
-  c.window_decorations = "RESIZE"
-  c.enable_tab_bar = false
+  c.window_decorations = "TITLE | RESIZE"
+  c.enable_tab_bar = true
 end
 
 return M
