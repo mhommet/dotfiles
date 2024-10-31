a885533f0503ab61c2a4403541ef999ae1abbfbd
Milan Hommet <mhommet@teicee.com>
2024-10-31 14:52:40 +0100

    m

--- wezterm/lua/appearance.lua
+++ wezterm/lua/appearance.lua
@@ -1,14 +1,12 @@
-local wez = require "wezterm"
-
 local M = {}
 
 M.apply_to_config = function(c)
   c.color_scheme = 'Catppuccin Mocha';
-  c.window_background_opacity = 1
+  c.window_background_opacity = 0.95
   c.inactive_pane_hsb = { brightness = 0.9 }
   c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
-  c.window_decorations = "TITLE | RESIZE"
-  c.enable_tab_bar = true
+  c.window_decorations = "NONE"
+  c.enable_tab_bar = false
 end
 
 return M
