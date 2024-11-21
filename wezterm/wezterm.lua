c6ea7ac000c74a21de3bcecf17c464384abf99d7
Milan Hommet <mhommet@teicee.com>
2024-11-21 16:32:16 +0100

    refactor: simplify wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,30 +1,25 @@
 local wez = require "wezterm"
-local utils = require "lua.utils"
-local mappings = require "lua.mappings"
-local appearance = require "lua.appearance"
-
 local c = {}
 
 if wez.config_builder then
   c = wez.config_builder()
 end
 
--- General configurations
+-- Appearance
 c.font = wez.font "JetBrainsMono Nerd Font"
-c.font_size = 12
+c.font_size = 13
+c.color_scheme = "Catppuccin Mocha"
+c.window_background_opacity = 0.8
+c.inactive_pane_hsb = { brightness = 0.9 }
+c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
+c.window_decorations = "NONE"
+c.enable_tab_bar = false
+
+-- General config
 c.adjust_window_size_when_changing_font_size = false
 c.audible_bell = "Disabled"
 c.scrollback_lines = 3000
 c.default_workspace = "main"
 c.status_update_interval = 2000
-
--- Clipboard
-
-
--- appearance
-appearance.apply_to_config(c)
-
--- keys
-mappings.setup_keys(c)
 
 return c
