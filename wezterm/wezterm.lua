013a39df6048fc28101d6e5732afe24efdaa0752
Milan Hommet <mhommet@teicee.com>
2024-11-22 14:59:28 +0100

    d

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,7 @@ end
 c.font = wez.font "JetBrainsMono Nerd Font"
 c.font_size = 13
 c.color_scheme = "Catppuccin Mocha"
-c.window_background_opacity = 0.8
+c.window_background_opacity = 0.9
 c.inactive_pane_hsb = { brightness = 0.9 }
 c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
 c.window_decorations = "NONE"
