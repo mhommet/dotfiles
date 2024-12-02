14af9760283b5b00555727d04386d0255789db27
Milan Hommet <mhommet@teicee.com>
2024-12-02 11:50:16 +0100

    fix transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,7 +12,7 @@ local scheme = wez.color.get_builtin_schemes()['rose-pine']
 c.colors = {
   split = scheme.ansi[2],
 }
-c.window_background_opacity = 0.9
+c.window_background_opacity = 0.95
 c.inactive_pane_hsb = { brightness = 0.9 }
 c.window_padding = { left = "1cell", right = "1cell", top = 0, bottom = 0 }
 c.window_decorations = "NONE"
