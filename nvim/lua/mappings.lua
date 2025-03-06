ba94d1bf7fb7b65dba8de8c32d854ae53e585c94
Milan Hommet <mhommet@teicee.com>
2025-03-06 14:13:33 +0100

    changing nvimTree shortcut to space + t cause its conflicting on space + e

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,7 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -8,8 +9,8 @@ map("i", "jk", "<ESC>")
 -- Select all with ctrl + a
 map("n", "<C-a>", "ggVG", { noremap = true })
 
--- Toggle Tree with space + e
-map("n", "<space>e", ":NvimTreeToggle<CR>", { noremap = true })
+-- Toggle Tree with space + t
+map("n", "<space>t", ":NvimTreeToggle<CR>", { noremap = true })
 
 -- Find file with space + space
 map("n", "<space><space>", ":Telescope find_files<CR>", { noremap = true })
