dcdd4b380d5759c9dfac6b463d6d4d85d97177f4
Milan Hommet <mhommet@teicee.com>
2025-03-18 10:34:52 +0100

    z

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DOTFILES_DIR=~/dotfiles
 CONFIG_DIR=~/.config
 
 # Liens de configuration principaux
-for dir in kitty fish nvim tmux; do
+for dir in kitty fish nvim; do
     TARGET=$CONFIG_DIR/$dir
     SOURCE=$DOTFILES_DIR/$dir
 
