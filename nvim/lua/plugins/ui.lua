bd922aca7a4e92bdbfe1bf8f6dfe64fc090c8265
Milan Hommet <mhommet@teicee.com>
2025-04-15 12:59:37 +0200

    feat: add mini notify and pair

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -73,6 +73,9 @@ return {
 			require("mini.ai").setup()
 			require("mini.surround").setup()
 			require("mini.files").setup()
+			require("mini.comment").setup()
+			require("mini.pairs").setup()
+			require("mini.notify").setup()
 		end,
 	},
 
