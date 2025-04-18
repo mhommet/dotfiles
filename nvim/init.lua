64335b5e3af07ab0b29b2761573c2a8a1609a34b
Milan Hommet <mhommet@teicee.com>
2025-04-18 17:38:52 +0200

    feat: using lazyvim distrib

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,2 @@
--- Measure startup time
-vim.g.startuptime_start = vim.fn.reltime()
-
-require('config.set')
-require('config.autocmd')
-require('config.lazy')
-require('config.remap')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,6 +2,14 @@ return {{
     "github/copilot.vim",
     event = "BufRead",
     cmd = "Copilot",
+    config = function()
+        -- Prioritise Copilot over nvim-cmp
+        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_assume_mapped = true
+        vim.g.copilot_tab_fallback = ""
+        
+        vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', { expr = true, silent = true })
+    end,
 },{
     "olimorris/codecompanion.nvim",
     dependencies = {
