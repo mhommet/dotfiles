f04cfe760eae6c1284cd692ab48959801da96b10
mhommet <milan.hommet@protonmail.com>
2025-07-24 15:12:31 +0200

    d

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -17,9 +17,6 @@ return {{
     },
     opts = {
         notify_on_error = false,
-        format_on_save = function(bufnr)
-            return false -- Disable format on save by default
-        end,
         formatters_by_ft = {
             lua = { 'stylua' },
             python = { 'black' },
