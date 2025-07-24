204a63164963d50080beb846fa08da73025a0e6b
mhommet <milan.hommet@protonmail.com>
2025-07-24 10:27:23 +0200

    adding visual guides for JCL files

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,13 +1,25 @@
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"*.cob", "*.cbl", "*.cpy"},
-  callback = function()
-    vim.bo.filetype = "cobol"
-  end,
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.cob", "*.cbl", "*.cpy" },
+    callback = function()
+        vim.bo.filetype = "cobol"
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "cobol",
-  callback = function()
-    vim.opt.colorcolumn = "7,72"
-  end,
+    pattern = "cobol",
+    callback = function()
+        vim.opt.colorcolumn = "7,72"
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.jcl",
+    callback = function()
+        vim.bo.filetype = "jcl"
+        vim.opt.colorcolumn = "3,11,73" -- Marge A (//), séparateur (col 11), fin de ligne
+        vim.cmd [[
+      syntax match jclMargeA "^//"
+      highlight jclMargeA guifg=#ff5c57 gui=bold
+    ]]
+    end
 })
