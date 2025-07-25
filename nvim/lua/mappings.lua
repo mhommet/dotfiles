2a55076877a167f2761e9bb13756796e8b213270
mhommet <milan.hommet@protonmail.com>
2025-07-25 13:08:29 +0200

    use mini.files as file explorer

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -19,3 +19,11 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window'
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
 
+-- Override nvchad nvim-tree mappings completely
+-- Force override the <leader>e mapping from nvchad after it loads
+vim.schedule(function()
+  pcall(vim.keymap.del, "n", "<leader>e")
+  vim.keymap.set("n", "<leader>e", function()
+    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+  end, { desc = "Open mini.files (Directory of Current File)" })
+end)
---
+++ nvim/lua/plugins/nvim-tree-disable.lua
@@ -0,0 +1,5 @@
+-- Disable nvim-tree completely
+return {
+  "nvim-tree/nvim-tree.lua",
+  enabled = false,
+} 
