54fff9a481a831279cd978748df5d3a1df1aacd2
mhommet <milan.hommet@protonmail.com>
2025-07-29 11:30:49 +0200

    chore: optimisations

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,25 @@
 require "nvchad.options"
 
--- add yours here!
+-- Options personnalisées
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Performance
+o.timeoutlen = 300         -- Temps d'attente pour les combinaisons de touches
+o.updatetime = 250         -- Temps de mise à jour pour les diagnostics
+o.redrawtime = 10000       -- Plus de temps pour les gros fichiers
+
+-- Interface
+o.scrolloff = 8            -- Garde 8 lignes visibles en haut/bas
+o.sidescrolloff = 8        -- Garde 8 colonnes visibles à gauche/droite
+o.cursorline = true        -- Highlight la ligne actuelle
+o.cursorlineopt = 'number' -- Highlight seulement le numéro de ligne
+
+-- Édition
+o.undofile = true          -- Sauvegarde l'historique d'annulation
+o.confirm = true           -- Demande confirmation avant de fermer un buffer modifié
+o.ignorecase = true        -- Recherche insensible à la casse
+o.smartcase = true         -- Mais sensible si majuscule dans la recherche
+
+-- Completion
+o.pumheight = 10           -- Limite la hauteur du menu de completion
+o.wildmode = "longest:full,full" -- Meilleure completion en ligne de commande
