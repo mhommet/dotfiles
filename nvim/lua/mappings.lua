b7f0c54ad637b355db9eefcb1508d03c27d5c490
mhommet <milan.hommet@protonmail.com>
2025-07-29 11:30:36 +0200

    refactor: use map for mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,16 +8,30 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Save with Ctrl+S
-vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, desc = 'Save file' })
+map('n', '<C-s>', ':w<CR>', { noremap = true, desc = 'Save file' })
 
 -- Select all
-vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, desc = 'Select all' })
+map('n', '<C-a>', 'ggVG', { noremap = true, desc = 'Select all' })
 
 -- [[ Window Navigation ]]
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+map('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+
+-- [[ Améliorer l'édition ]]
+-- Déplacement de lignes
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Meilleure navigation dans les wraps
+map("n", "j", "gj", { desc = "Down (wrapped)" })
+map("n", "k", "gk", { desc = "Up (wrapped)" })
+
+-- Effacer le highlight de recherche
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 -- Override nvchad nvim-tree mappings completely
 -- Force override the <leader>e mapping from nvchad after it loads
