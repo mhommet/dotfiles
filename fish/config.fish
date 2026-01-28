11a540dcf1503acf9af433ce2118cc10733d02d7
Milan Hommet <milan.hommet@protonmail.com>
2026-01-28 15:30:01 +0100

    e

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,18 @@ starship init fish | source
 # Remove default fish greeting
 set fish_greeting
 
+fish_add_path -m ~/.local/bin
+
+set -gx HSA_OVERRIDE_GFX_VERSION 12.0.0
+set -gx HSA_ENABLE_SDMA 1
+set -gx PATH /opt/rocm/bin $PATH
+
+# LiteLLM Proxy (fix 404)
+set -gx ANTHROPIC_AUTH_TOKEN ollama
+set -gx ANTHROPIC_BASE_URL http://localhost:4000/v1  # LiteLLM !
+set -gx ANTHROPIC_API_KEY ""
+
+# Pipx PATH
+set PATH $PATH /home/milan/.local/bin
+
+fish_config theme choose "Rosé Pine Auto"
--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "aura-dark",
+	theme = "rosepine",
   transparency = true
 }
 
